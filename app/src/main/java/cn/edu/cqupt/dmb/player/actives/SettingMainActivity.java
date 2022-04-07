5f274637ba93a8cd7d262b82f23050f9eaa8b99a
Gouzhong1223 <1162864960@qq.com>
2022-04-07 16:27:53 +0800

    新增了一个设置页面的 Activity

---
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/SettingMainActivity.java
@@ -0,0 +1,19 @@
+package cn.edu.cqupt.dmb.player.actives;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.cqupt.dmb.player.R;
+
+/**
+ * 这是设置页面的 Activity
+ */
+public class SettingMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_main);
+    }
+}
