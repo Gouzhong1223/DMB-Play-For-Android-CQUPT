b338dd17ea7514ea8c217160331b6b74e9ba6218
Gouzhong1223 <1162864960@qq.com>
2022-04-07 16:28:33 +0800

    新增了一个课表显示的 Activity

---
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/CurriculumActivity.java
@@ -0,0 +1,19 @@
+package cn.edu.cqupt.dmb.player.actives;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.cqupt.dmb.player.R;
+
+/**
+ * 这个是显示课表的 Activity
+ */
+public class CurriculumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curriculum);
+    }
+}
