e88e53677bcc303f188922c51cebeb0bf69fbc6c
Gouzhong1223 <1162864960@qq.com>
2022-04-07 16:59:25 +0800

    完善了一下课表的播放逻辑

--- app/src/main/java/cn/edu/cqupt/dmb/player/actives/CurriculumActivity.java
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/CurriculumActivity.java
@@ -1,19 +1,46 @@
 package cn.edu.cqupt.dmb.player.actives;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import cn.edu.cqupt.dmb.player.R;
+import cn.edu.cqupt.dmb.player.decoder.TpegDecoderImprovement;
+import cn.edu.cqupt.dmb.player.listener.DmbCurriculumListener;
 
 /**
  * 这个是显示课表的 Activity
  */
 public class CurriculumActivity extends AppCompatActivity {
 
+    private static final String TAG = "CurriculumActivity";
+    private ImageView imageView;
+    private TpegDecoderImprovement tpegDecoderImprovement;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curriculum);
+        initView();
+        startPlayCurriculum();
+    }
+
+    private void initView() {
+        Log.i(TAG, "正在初始化课表显示组件");
+        imageView = findViewById(R.id.curriculum_image_view);
+    }
+
+    private void startPlayCurriculum() {
+        DmbCurriculumListener dmbCurriculumListener = new DmbCurriculumListener(imageView);
+        tpegDecoderImprovement = new TpegDecoderImprovement(dmbCurriculumListener);
+        tpegDecoderImprovement.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        tpegDecoderImprovement.interrupt();
+        super.onDestroy();
     }
 }
--- app/src/main/java/cn/edu/cqupt/dmb/player/listener/DmbCurriculumListener.java
+++ app/src/main/java/cn/edu/cqupt/dmb/player/listener/DmbCurriculumListener.java
@@ -1,5 +1,13 @@
 package cn.edu.cqupt.dmb.player.listener;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.ImageView;
+
+import cn.edu.cqupt.dmb.player.common.FrequencyModule;
+import cn.edu.cqupt.dmb.player.utils.DataReadWriteUtil;
+
 /**
  * @Author : Gouzhong
  * @Blog : www.gouzhong1223.com
@@ -12,9 +20,48 @@ package cn.edu.cqupt.dmb.player.listener;
  * @Version : 1.0.0
  */
 public class DmbCurriculumListener implements DmbListener {
+
+    private static final String TAG = "DmbCurriculumListener";
+
+    /**
+     * 显示课表的 ImageView
+     */
+    private final ImageView imageView;
+
+    /**
+     * 文件缓冲区
+     */
+    private final byte[] fileBuffer = new byte[1024 * 1024 * 2];
+
+    public DmbCurriculumListener(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
     @Override
     public void onSuccess(String fileName, byte[] bytes, int length) {
-
+        FrequencyModule frequencyModule = DataReadWriteUtil.getFrequencyModule();
+        if (frequencyModule == null) {
+            Log.e(TAG, "出错啦!现在没有设置活跃的组件,所以这里的回调方法就直接抛弃!");
+            return;
+        }
+        if (!frequencyModule.getModuleName().startsWith("CURRICULUM")) {
+            // 活跃组件不是课表,直接返回
+            Log.e(TAG, "当前活跃的组件不是课表");
+            return;
+        }
+        String needBuilding = frequencyModule.getModuleName().split("-")[1];
+        if (!fileName.contains(needBuilding)) {
+            // 如果不是需要的课表,就直接返回
+            return;
+        }
+        System.arraycopy(bytes, 0, fileBuffer, 0, length);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(fileBuffer, 0, length);
+        if (bitmap != null) {
+            Log.i(TAG, "重新设置了一下课表图片");
+            imageView.setImageBitmap(bitmap);
+        } else {
+            Log.e(TAG, "生成 bitmap 错误啦!");
+        }
     }
 
     @Override
