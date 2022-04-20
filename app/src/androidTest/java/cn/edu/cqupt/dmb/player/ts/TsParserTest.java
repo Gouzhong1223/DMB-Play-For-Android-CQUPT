1a0c142d0eb5a71190b93224373df6799b8f8c9f
Gouzhong1223 <1162864960@qq.com>
2022-04-20 16:55:14 +0800

    TsParserTest 单元测试

---
+++ app/src/androidTest/java/cn/edu/cqupt/dmb/player/ts/TsParserTest.java
@@ -0,0 +1,34 @@
+package cn.edu.cqupt.dmb.player.ts;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description :
+ * @Date : create by QingSong in 2022-04-19 12:28
+ * @Email : qingsong.qs@alibaba-inc.com
+ * @Since : JDK 1.8
+ * @PackageName : cn.edu.cqupt.dmb.player.ts
+ * @ProjectName : DMB Player For Android
+ * @Version : 1.0.0
+ */
+public class TsParserTest {
+
+    private static final String TAG = "TsParserTest";
+
+    @Test
+    public void feedTSData() {
+        Log.i(TAG, "feedTSData: Test");
+    }
+
+    @Test
+    public void getMalFormedChannels() {
+    }
+
+    @Test
+    public void resetDataVersions() {
+    }
+}
