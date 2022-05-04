65ef8446e0652e242daa0461efd8325c257a4a22
Gouzhong1223 <1162864960@qq.com>
2022-05-04 21:51:56 +0800

    add Dangle 类型 枚举

---
+++ app/src/main/java/cn/edu/cqupt/dmb/player/common/DangleType.java
@@ -0,0 +1,18 @@
+package cn.edu.cqupt.dmb.player.common;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description :
+ * @Date : create by QingSong in 2022-05-04 21:41
+ * @Email : qingsong.qs@alibaba-inc.com
+ * @Since : JDK 1.8
+ * @PackageName : cn.edu.cqupt.dmb.player.common
+ * @ProjectName : DMB Player For Android
+ * @Version : 1.0.0
+ */
+public enum DangleType {
+    STM32,
+    NUC,
+    AT,
+}
