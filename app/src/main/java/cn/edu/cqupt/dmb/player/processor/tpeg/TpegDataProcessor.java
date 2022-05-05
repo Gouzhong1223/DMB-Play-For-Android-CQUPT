31bd3be4b110346440e4362be738800d8c647865
Gouzhong1223 <1162864960@qq.com>
2022-05-05 14:31:57 +0800

    新增 TPEG 帧数据处理器

---
+++ app/src/main/java/cn/edu/cqupt/dmb/player/processor/tpeg/TpegDataProcessor.java
@@ -0,0 +1,16 @@
+package cn.edu.cqupt.dmb.player.processor.tpeg;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description : TPEG 数据帧的数据处理器接口
+ * @Date : create by QingSong in 2022-05-05 14:26
+ * @Email : qingsong.qs@alibaba-inc.com
+ * @Since : JDK 1.8
+ * @PackageName : cn.edu.cqupt.dmb.player.processor.tpeg
+ * @ProjectName : DMB Player For Android
+ * @Version : 1.0.0
+ */
+public interface TpegDataProcessor {
+    void processData();
+}
