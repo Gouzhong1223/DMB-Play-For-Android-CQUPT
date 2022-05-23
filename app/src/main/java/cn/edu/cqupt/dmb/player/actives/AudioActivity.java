b83c2fda6a65929860d1e2b8d614715221bf14c0
Gouzhong1223 <1162864960@qq.com>
2022-05-23 14:22:33 +0800

    feat(activity): 添加音频播放的 Activity

---
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/AudioActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.cqupt.dmb.player.actives;
+
+import android.os.Bundle;
+
+import cn.edu.cqupt.dmb.player.R;
+
+public class AudioActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+    }
+}
