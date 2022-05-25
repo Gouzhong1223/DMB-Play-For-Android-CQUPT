885ef0784d7959b2c7fd2fe6150a5f6689783984
Gouzhong1223 <1162864960@qq.com>
2022-05-25 18:28:04 +0800

    rename activity_dormitory

--- app/src/main/java/cn/edu/cqupt/dmb/player/actives/DormitorySafetyActivity.java
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/DormitorySafetyActivity.java
@@ -19,7 +19,7 @@ public class DormitorySafetyActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sushe);
+        setContentView(R.layout.activity_dormitory);
         initView();
     }
 
