1efd7168e577f77ca011fc2a600073b921b7c252
Gouzhong1223 <1162864960@qq.com>
2022-05-26 14:48:59 +0800

    rename banner id

--- app/src/main/java/cn/edu/cqupt/dmb/player/actives/DormitorySafetyActivity.java
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/DormitorySafetyActivity.java
@@ -1,6 +1,8 @@
 package cn.edu.cqupt.dmb.player.actives;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.youth.banner.Banner;
 import com.youth.banner.indicator.CircleIndicator;
@@ -14,17 +16,23 @@ import cn.edu.cqupt.dmb.player.banner.bean.BannerDataBean;
  */
 public class DormitorySafetyActivity extends BaseActivity {
 
+    /**
+     * 宿舍安全信息中间的轮播图
+     */
     private Banner banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 强制全屏,全的不能再全的那种了
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dormitory);
         initView();
     }
 
     private void initView() {
-        banner = findViewById(R.id.banner);
+        banner = findViewById(R.id.dormitory_banner);
         useBanner();
     }
 
@@ -40,6 +48,9 @@ public class DormitorySafetyActivity extends BaseActivity {
 
     @Override
     protected void onDestroy() {
+        if (banner != null) {
+            banner.destroy();
+        }
         super.onDestroy();
     }
 }
