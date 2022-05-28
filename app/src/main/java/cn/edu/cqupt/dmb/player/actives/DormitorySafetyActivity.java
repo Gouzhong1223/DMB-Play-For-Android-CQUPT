45c28fe1fbdc2db3f7eff287dcf7e0c85e68102f
Gouzhong1223 <1162864960@qq.com>
2022-05-28 23:17:29 +0800

    完善宿舍安全信息界面设计

--- app/src/main/java/cn/edu/cqupt/dmb/player/actives/DormitorySafetyActivity.java
+++ app/src/main/java/cn/edu/cqupt/dmb/player/actives/DormitorySafetyActivity.java
@@ -1,5 +1,8 @@
 package cn.edu.cqupt.dmb.player.actives;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.youth.banner.Banner;
 import com.youth.banner.indicator.CircleIndicator;
 
@@ -12,6 +15,7 @@ import cn.edu.cqupt.dmb.player.banner.bean.BannerDataBean;
  */
 public class DormitorySafetyActivity extends BaseActivity {
 
+    private static final String TAG = "DormitorySafetyActivity";
     /**
      * 宿舍安全信息中间的轮播图
      */
@@ -24,7 +28,12 @@ public class DormitorySafetyActivity extends BaseActivity {
     }
 
     @Override
-    protected void startDecode() {
+    public void configView() {
+        Log.i(TAG, "configView: ");
+    }
+
+    @Override
+    public void startDecode() {
 
     }
 
@@ -34,6 +43,7 @@ public class DormitorySafetyActivity extends BaseActivity {
     public void useBanner() {
         //添加生命周期观察者
         banner.addBannerLifecycleObserver(this).setAdapter(new ImageAdapter(BannerDataBean.getHelloViewData())).setIndicator(new CircleIndicator(this));
+        banner.setBannerRound2(convertDpToPixel(this, 30));
     }
 
     @Override
@@ -42,5 +52,10 @@ public class DormitorySafetyActivity extends BaseActivity {
             banner.destroy();
         }
         super.onDestroy();
+    }
+
+    private int convertDpToPixel(Context context, int dp) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return Math.round((float) dp * density);
     }
 }
